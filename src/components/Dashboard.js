da8ee723ec29c425084cf3d0bcde2e0cbfc5f268
Xo'jayev Yo'ldoshbek <xojayevyoldoshbek@Yoldoshbeks-MacBook-Pro.local>
2021-08-07 17:55:05 +0500

    slight theme is available for sign in page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,9 @@
-import {Link} from 'react-router-dom'
-export default function Dashboard(){
+//import {Link} from 'react-router-dom'
+import LeftMenu from './LeftMenu'
+export default function Dashboard(props){
     return(
         <div>
-            <Link to='/signin'>Sign in </Link>
-            <Link to='/signup'>Sign up </Link>
-            <Link to='/forgotpassword'>Forgot password</Link>
+            <LeftMenu isDarkTheme={props.isDarkTheme}/>
         </div>
     )
 }
